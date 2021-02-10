79d6f771685d19b13e11773723f910b5e6e8ced6
WesIngwersen <ingwersen.wesley@epa.gov>
2021-02-10 13:59:33 -0500

    -Add write_df_to_file -Add common var for supported extensions -Clean up docstrings

--- esupy/processed_data_mgmt.py
+++ esupy/processed_data_mgmt.py
@@ -2,19 +2,20 @@
 # !/usr/bin/env python3
 # coding=utf-8
 """
-Functions to manage querying, retrieving and storing preprocessed data in local directories
-Currently assumes parquet format
+Functions to manage querying, retrieving and storing of preprocessed data in local directories
 """
 import logging as log
 import os
 import pandas as pd
 import re
 #from esupy.remote import make_http_request
-#from esupy.util import strip_file_extension
+from esupy.util import supported_ext
 import appdirs
+
 
 class Paths:
     local_path = appdirs.user_data_dir()
+
 
 class FileMeta:
     tool = ""
@@ -22,6 +23,8 @@ class FileMeta:
     name_data = ""
     tool_version = ""
     git_hash = ""
+    ext = ""
+
 
 def load_preprocessed_output(file_meta, paths):
     """
@@ -37,6 +40,7 @@ def load_preprocessed_output(file_meta, paths):
     else:
         return None
 
+
 def find_file(meta,paths,force_version=False):
     """
     Searches for file within path.local_path based on file metadata
@@ -51,9 +55,12 @@ def find_file(meta,paths,force_version=False):
         search_words = meta.name_data
         #Get a list of all matching files
         r = list(filter(lambda x: re.search(search_words, x), fs))
+        #Filter by extensions using common supported_ext
+        #r = list(filter(lambda x: re.search(supported_ext, x), r))
         if len(r)==0:
             f = ""
         elif len(r)>1:
+            #temporary selection to grab the first file in the list
             f = os.path.realpath(path + "/" + r[0])
         else:
             f = os.path.realpath(path + "/" + r[0])
@@ -61,6 +68,33 @@ def find_file(meta,paths,force_version=False):
         f = ""
     return f
 
+
+def write_df_to_file(df,paths,meta):
+    """
+    Writes a data frame to the designated local folder and file name created using paths and meta
+    :param df: a pandas dataframe
+    :param paths: populated instance of class Paths
+    :param meta: populated instance of class FileMeta
+    :return: None
+    """
+    folder = os.path.realpath(paths.local_path + "/" + meta.category)
+    file = folder + "/" + meta.name_data + "_" + meta.tool_version
+    if meta.git_hash is not None:
+        file = file + "_" + meta.git_hash
+    try:
+        create_paths_if_missing(file)
+        if meta.ext=="parquet":
+            file = file + ".parquet"
+            file = os.path.realpath(file)
+            df.to_parquet(file, engine="pyarrow")
+        elif meta.ext == "csv":
+            file = file + ".csv"
+            file = os.path.realpath(file)
+            df.to_csv(file, index=False)
+        else:
+            log.error('Failed to save ' + file + '. ' + "Meta data lacks 'ext' property")
+    except:
+        log.error('Failed to save '+ file + '.')
 
 def read_into_df(file):
     """
@@ -91,4 +125,3 @@ def create_paths_if_missing(file):
     dir = os.path.dirname(file)
     if not os.path.exists(dir):
         os.makedirs(dir)
-
