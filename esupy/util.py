67b9a76e6ff9b49023ab95495d2f49540ca03829
Ben Young <Ben.Young@erg.com>
2021-07-29 10:25:07 -0400

    add function to access git hash of current directory

--- esupy/util.py
+++ esupy/util.py
@@ -4,6 +4,9 @@
 """
 Simple utility functions for reuse in tools
 """
+import os
+import subprocess
+
 supported_ext = ["parquet","csv"]
 
 
@@ -16,3 +19,23 @@ def strip_file_extension(filename):
     return(filename.rsplit(".", 1)[0])
 
 
+def is_git_directory(path = '.'):
+    """
+    Returns True if path contains git directory
+    """
+    return subprocess.call(['git', '-C', path, 'status'],
+                           stderr=subprocess.STDOUT,
+                           stdout = open(os.devnull, 'w')) == 0
+
+def get_git_hash():
+    """
+    Returns 7 digit git_hash of current directory or None if no git found
+    """
+    git_hash = None
+    if is_git_directory():
+        try:
+            git_hash = subprocess.check_output(
+                ['git', 'rev-parse', 'HEAD']).strip().decode(
+                'ascii')[0:7]
+        except: pass
+    return git_hash
