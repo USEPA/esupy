3e6ef33602b19ab42d81db0f524e7b125c9199dd
a-w-beck <andrew.beck@erg.com>
2021-12-17 14:55:08 -0500

    Full detail shapefiles (replacing simplified CB shapefiles) for data years 2008-2021 now retrieved from Census servers and intersected with point data. Data documentation compiled into markdown. Points are now assigned urban/rural/unspecified labels.

--- esupy/urban_geo_classify.py
+++ esupy/urban_geo_classify.py
@@ -3,8 +3,10 @@
 # coding=utf-8
 
 """
-Module to classify lat/long pairs as lying w/i urban or rural Census block area
+Module to classify lat/long pairs as lying within urban or rural Census block area
 """
+import yaml
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 import shapely.geometry as sgm
@@ -12,31 +14,78 @@ from shapely.prepared import prep
 from pathlib import Path
 
 datapath = Path(__file__).parent/'census_shp'
-# print(datapath)
 
-def urb_intersect(gdf_pt, year):
+def urb_intersect(df_pt, year):
     """
-    Classify lat/long points as urban/rural via intersection with 
-    urban polygons from a specified data year {2000, 2010}.
-    :param gdf_pt: GeoPandas GeoDatFrame with point geometry values
+    Classify lat/long points as urban, rural, or unspecified 
+    via intersection with urban polygons from a user-specified data year.
+    :param df: pandas dataframe, with Latitude and Longitude cols
     :param year: data year of Census urban area/cluster polygons
     """
-    urb_2000 = read_census_shp(datapath, "ua99_d00_shp.zip")
-    urb_2010 = read_census_shp(datapath, "cb_2012_us_uac10_500k.zip")
-    urb_2000.crs = urb_2010.crs  # 2000 cb crs missing; inherit 2010 cb crs
+    gdf_urb = get_census_shp(year)
+    mpu = multipoly_agg(gdf_urb) # aggregate to avoid element-wise comparison
+    mpu = prep(mpu)  # shapely's quick spatial indexing fxn
     
-    if year == 2000:
-        gdf_urb = crs_harmonize(urb_2000)  # convert to WGS84
-    elif year == 2010:
-        gdf_urb = crs_harmonize(urb_2010)  # convert to WGS84
-    else: 
-        print(f"Data year {year} unavailable")
-        return None
-    
-    mpu = multipoly_agg(gdf_urb)
-    mpu = prep(mpu)  # shapely's quick spatial indexing fn
+    gdf_pt = parse_pt_data(df_pt)
     gdf_pt['urban'] = gdf_pt['geometry'].apply(lambda x: mpu.intersects(x))
+    
+    # classify 'unspecified' via Lat & Long cols (gpd na/empty check fns won't catch)
+    cond = [(gdf_pt['Latitude'].isna() | gdf_pt['Longitude'].isna()),
+            (gdf_pt['urban'] == True),
+            (gdf_pt['urban'] == False)]
+    vals = ['unspecified', 'urban', 'rural']    
+    gdf_pt['urb_class'] = np.select(cond, vals)
     return gdf_pt
+
+def get_census_shp(year):
+    """
+    Read in shapefile as gpd geodataframe. 
+    Refer to census_shp/README.md for explanation of encoding errors 
+    thrown by gpd.read_file() for pre-2015 SHPs.    
+    :param datapath: pathlib Path pointing to shapefile dir
+    :param filename: file name string with extension
+    """ 
+    with open(datapath / 'census_uac_urls.yaml', 'r') as f:
+        uac_url = yaml.safe_load(f)
+        
+    time_get = time.time()
+    
+    try:     # may also return an HTTPError from urllib        
+        print(f'Retrieving {year} UAC shapefile from {uac_url[year]}')
+        if year in [2010, 2011]:  # data years rely on 2x SHP's
+            gdf0 = gpd.read_file(uac_url[year][0])
+            gdf1 = gpd.read_file(uac_url[year][1])
+            gdf = gdf0.append(gdf1)
+            if not gdf.crs == gdf0.crs == gdf1.crs:
+                print(f'WARNING Combined {year} gdf CRS is inconsistent')
+        else: 
+            gdf = gpd.read_file(uac_url[year])
+            # gdf = gpd.read_file(uac_url[year], encoding = 'iso-8859-1')
+    except KeyError:
+        print(f'Census urban area data year {year} unavailable')
+    
+    print(f'[get SHP] --- {time.time() - time_get} seconds ---')
+    
+    gdf = crs_harmonize(gdf)  # convert to WGS84
+    
+    # check for empty/na geom values
+    ena = sum(gdf['geometry'].is_empty | gdf['geometry'].isna())
+    if ena > 0: print(f"{year} Census SHP has {ena} empty + NA geometries")
+    return gdf
+
+def parse_pt_data(df):
+    """
+    Convert df containing Lat & Long columns to gpd.GeoDataFrame
+    with geometry column of Points
+    :param df: pandas dataframe
+    """
+    # check for na lat/long values
+    pt_na = sum(df['Latitude'].isna() | df['Longitude'].isna()) 
+    if pt_na != 0: print(f"Point data contains {pt_na} nan Lat and/or Long values")
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+    gdf = crs_harmonize(gdf)
+    return gdf
 
 def multipoly_agg(gdf):
     """
@@ -46,47 +95,19 @@ def multipoly_agg(gdf):
     """
     gdf_mp = gdf[gdf['geometry'].type == 'MultiPolygon'] # important to test this piece on urb_2010
     gdf_p = gdf[gdf['geometry'].type == 'Polygon']
-    
     if len(gdf) > (len(gdf_mp) + len(gdf_p)):
         print('GeoDataFrame invalid; contains non-polygon geometry entries')
         return None
-    
-    # extract polygons from multipolygons & concatenate into list
+    # multipolygons are collections of polygons; extract & concatenate into list
     mp = [poly for multipoly in gdf_mp['geometry'] for poly in multipoly]
-    
-    p = list(gdf_p['geometry']) # concatenate polygons into a list    
-    multipoly = sgm.MultiPolygon(mp + p)  # join lists & convert to mp
+    p = list(gdf_p['geometry']) # concatenate single polygons into a list    
+    multipoly = sgm.MultiPolygon(mp + p)  # join lists & convert to single mp
     return multipoly
-
-def read_census_shp(datapath, filename):
-    """
-    Read in shapefile as gpd geodataframe.
-    :param datapath: pathlib Path pointing to shapefile dir
-    :param filename: file name string with extension
-    """ 
-    gdf = gpd.read_file(datapath / filename)
-    # check for empty/na geom values
-    a = sum(gdf.geometry.is_empty | gdf.geometry.isna())
-    if a != 0: print(f"{filename} contains {a} empty and/or NA geometries")
-    return gdf
-
-def read_point_data(df):
-    """
-    Convert df containing Lat & Long columns to gpd.GeoDataFrame
-    with geometry column
-    :param df: pandas dataframe
-    """
-    # TO DO: thorough review of lat/long headers
-        # identify & modify w/ regex on df.columns? 
-        # or just a dictionary across sources?
-    gdf = gpd.GeoDataFrame(
-        df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
-    return gdf
 
 def crs_harmonize(gdf):
     """
     Convert coordinate reference systems (CRS) to WGS84 (EPSG:4326), 
-    and assign WGS84 as assumed default when missing.
+    or if missing, assume WGS84 and assign.
     :param gdf: GeoPandas geodataframe object
     """
     WGS84 = "EPSG:4326"  # EPSG code
@@ -98,13 +119,11 @@ def crs_harmonize(gdf):
     
 
 if __name__ == "__main__":  
-    import time    
-    start_time = time.time()
     import stewi
-    tri_raw = stewi.getInventoryFacilities('TRI',2019)
-    tri_pt = read_point_data(tri_raw)
-    tri_pt = crs_harmonize(tri_pt)
-    tri_urb = urb_intersect(tri_pt, 2010)
-    print("--- %s seconds ---" % (time.time() - start_time)) 
-
-
+    import time
+    time_start = time.time()
+    year = 2010 
+    pt_raw = stewi.getInventoryFacilities('TRI',year)  # ('TRI', 2019)
+    pt_urb = urb_intersect(pt_raw, year)
+    print(f'[total] --- {time.time() - time_start} seconds ---')
+    
