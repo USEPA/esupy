0d22b24a3cc21c383c28b6c26cffb8e4e18f5225
Ben Young <Ben.Young@erg.com>
2022-07-27 22:03:29 -0400

    revise mapping to make more flexible for ignoring fields

--- esupy/mapping.py
+++ esupy/mapping.py
@@ -13,7 +13,7 @@ def apply_flow_mapping(df, source, flow_type, keep_unmapped_rows=False,
     """
     Maps a dataframe using a flow mapping file from fedelemflowlist or
     materialflowlist.
-    
+
     :param df: dataframe to be mapped
     :param source: list or str, name of mapping file(s)
     :param flow_type: str either 'ELEMENTARY_FLOW', 'TECHNOSPHERE_FLOW',
@@ -31,7 +31,7 @@ def apply_flow_mapping(df, source, flow_type, keep_unmapped_rows=False,
         'Unit','Context','FlowAmount','FlowUUID'
     :param ignore_source_name: bool, False if flows should be mapped based on
         SourceName. (E.g., should be False when mapping across multiple datasets)
-        
+
     """
 
     if field_dict is None:
@@ -42,7 +42,7 @@ def apply_flow_mapping(df, source, flow_type, keep_unmapped_rows=False,
                       'FlowableContext':'Context',
                       'FlowableQuantity':'FlowAmount',
                       'UUID':'FlowUUID'}
-    
+
     mapping_fields = ["SourceListName",
                       "SourceFlowName",
                       "SourceFlowContext",
@@ -82,8 +82,8 @@ def apply_flow_mapping(df, source, flow_type, keep_unmapped_rows=False,
     if len(mapping) == 0:
         # mapping not found
         return None
-    
-    mapping = mapping[mapping_fields]    
+
+    mapping = mapping[mapping_fields]
     mapping[['ConversionFactor']] = mapping[['ConversionFactor']].fillna(
         value=1)
     if keep_unmapped_rows is False:
@@ -91,39 +91,52 @@ def apply_flow_mapping(df, source, flow_type, keep_unmapped_rows=False,
     else:
         merge_type = 'left'
 
-    map_to = [field_dict['SourceName'],
-              field_dict['FlowableName'],
-              field_dict['FlowableContext'],
-              field_dict['FlowableUnit']]
-    
+    map_to = [field_dict.get('SourceName'),
+              field_dict.get('FlowableName'),
+              field_dict.get('FlowableContext'),
+              field_dict.get('FlowableUnit')]
+
     map_from = ["SourceListName",
                 "SourceFlowName",
                 "SourceFlowContext",
                 "SourceUnit"]
-    
+
     if ignore_source_name:
         map_to.remove(field_dict['SourceName'])
         map_from.remove('SourceListName')
-    
-    for field in map_to:
-        df[field].fillna('', inplace=True)
-    for field in map_from:
-        mapping[field].fillna('', inplace=True)
 
-    # merge df with flows    
+    del_list = []
+    for i in range(len(map_to)):
+        if map_to[i] is not None:
+            df[map_to[i]].fillna('', inplace=True)
+            mapping[map_from[i]].fillna('', inplace=True)
+        else:
+            del_list.append(i)
+    del_list.reverse()
+    for x in del_list:
+        del map_to[x]
+        del map_from[x]
+
+    # merge df with flows
     mapped_df = pd.merge(df, mapping,
                              left_on=map_to,
                              right_on=map_from,
                              how=merge_type)
-    
+
     criteria = mapped_df['TargetFlowName'].notnull()
-    
-    mapped_df.loc[criteria, field_dict['FlowableName']] = mapped_df["TargetFlowName"]
-    mapped_df.loc[criteria, field_dict['FlowableContext']] = mapped_df["TargetFlowContext"]
-    mapped_df.loc[criteria, field_dict['FlowableUnit']] = mapped_df["TargetUnit"]
+
+    replacement_dict = {'FlowableName': 'TargetFlowName',
+                        'FlowableContext': 'TargetFlowContext',
+                        'FlowableUnit': 'TargetUnit',
+                        'UUID': 'TargetFlowUUID'}
+
+    for k, v in replacement_dict.items():
+        try:
+            mapped_df.loc[criteria, field_dict[k]] = mapped_df[v]
+        except KeyError:
+            pass # Not mapping on that field
     mapped_df.loc[criteria, field_dict["FlowableQuantity"]] = \
         mapped_df[field_dict["FlowableQuantity"]] * mapped_df["ConversionFactor"]
-    mapped_df.loc[criteria, field_dict['UUID']] = mapped_df["TargetFlowUUID"]
 
     # drop mapping fields
     mapped_df = mapped_df.drop(columns=mapping_fields)
