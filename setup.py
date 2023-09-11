478000832530c18a04c5b3881a7fb099fc225d49
Ben Young <Ben.Young@erg.com>
2023-09-11 11:26:53 -0400

    :bookmark: bump to v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='esupy',
-    version='0.3.0',
+    version='0.3.1',
     packages=['esupy'],
     include_package_data=True,
     python_requires=">=3.7",
