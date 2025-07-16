d5098c943afd3afc4fc16fd765251cbf1b75ad2b
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-07-16 16:43:11 -0600

    return python package version function

--- esupy/util.py
+++ esupy/util.py
@@ -75,3 +75,24 @@ def make_uuid(*args: str) -> str:
     """
     path = as_path(*args)
     return str(uuid.uuid3(uuid.NAMESPACE_OID, path))
+
+
+def return_pkg_version(MODULEPATH, packagename):
+    """
+    Return package version
+    :param MODULEPATH: str, package path
+    :param packagename: str, such as "flowsa"
+    """
+    # return version with git describe
+    try:
+        # set path to package repository, necessary if running method files
+        # outside the package repo
+        tags = subprocess.check_output(
+            ["git", "describe", "--tags", "--always", "--match", "v[0-9]*"],
+            cwd=MODULEPATH).decode().strip()
+        print(tags)
+        pkg_version = tags.split("-", 1)[0].replace('v', "")
+    except subprocess.CalledProcessError:
+        pkg_version = version(packagename)
+
+    return pkg_version
